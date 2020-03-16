a97e69a6847376b5b03278b76c78249a2e931b2b
dimbag-alex <57632006+dimbag-alex@users.noreply.github.com>
2020-03-16 18:23:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from wtforms.validators import DataRequired
 from data import db_session
 from data.users import User
 from data import news_api
-from data import news_rescources
+from data import news_resources
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -23,10 +23,10 @@ url = "http://127..0.0.1:8080/"
 
 
 # для списка объектов
-api.add_resource(news_rescources.UsersListResource, '/api/v2/news')
+api.add_resource(news_resources.UsersListResource, '/api/v2/news')
 
 # для одного объекта
-api.add_resource(news_rescources.UsersResource, '/api/v2/news/<int:news_id>')
+api.add_resource(news_resources.UsersResource, '/api/v2/news/<int:news_id>')
 
 
 if __name__ == '__main__':
