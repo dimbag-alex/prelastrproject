a21a8dcab973af809b4113d2bab609853485aaa5
dimbag-alex <57632006+dimbag-alex@users.noreply.github.com>
2020-03-16 18:13:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,26 +9,25 @@ from wtforms import PasswordField, BooleanField, SubmitField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
-import news_resources
 from data import db_session
-from data import news_api
 from data.users import User
+from data import news_api
+from data import news_rescources
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init("db/mars_exploers.db")
-login_manager = LoginManager()
-login_manager.init_app(app)
+app = Flask(__name__)
 api = Api(app)
-url = "http://127.0.0.1:8080/"
-
+url = "http://127..0.0.1:8080/"
 
 
 # для списка объектов
-api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+api.add_resource(news_rescources.UsersListResource, '/api/v2/news')
 
 # для одного объекта
-api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
+api.add_resource(news_rescources.UsersResource, '/api/v2/news/<int:news_id>')
+
 
 if __name__ == '__main__':
     app.run()
